7efbf618538947d75d2b398ee58d2a2b570ec278
Teodora <tedyted26@hotmail.com>
2024-05-24 10:07:38 +0200

    comit

--- start_app.sh
+++ start_app.sh
@@ -5,25 +5,29 @@ function exit_err {
     exit 1
 }
 
-while getopts ":d:i:c:h:e:xg:f:" option; do
+PORT=8080
+USERNAME="ucloud"
+PASSWORD="ucloud"
+
+while getopts ":d:i:c:h:e:xg:f:p:u:s:" option; do
     case "$option" in
         d) PGDATA_DIR=${OPTARG};;
         i) INITIALIZATION="$OPTARG" ;;
         c) CONFIGURATION="$OPTARG" ;;
         h) AIRFLOW_HOME=${OPTARG};;
         e) AIRFLOW__CORE__EXECUTOR="$OPTARG";;
-        # FIXME: By default is true, so maybe set it to false here or put it up
-        x) AIRFLOW__CORE__LOAD__EXAMPLES="True";; 
-        g) AIRFLOW__CORE__DAGS_FOLDER=${OPTARG};;
+        x) AIRFLOW__CORE__LOAD__EXAMPLES="False";; 
+        g) AIRFLOW__CORE__DAGS_FOLDER="$OPTARG";;
         f) AIRFLOW__CORE__FERNET_KEY="$OPTARG";;
-        # FIXME: add a parameter for providers?
+        p) PORT="$OPTARG";;
+        u) USERNAME="$OPTARG";;
+        s) PASSWORD="$OPTARG";;
+        # Add note too docs
         # would have to run again $ pip install "apache-airflow==2.9.1" apache-airflow-providers-google==10.1.0
         # This is to ensure pip doesnt upgrade/downgrade airflow by accident
         :) exit_err "Missing argument for -$OPTARG" ;;
         *) exit_err "Invalid option -$OPTARG" ;;
-        # FIXME: add celery result backend? by default is the metadata DB (postgres)
-        # FIXME: add webserver secret key?
-        # FIXME: add webserver_config.py?
+        # Add webserver_config.py?
     esac
 done
 
@@ -83,68 +87,70 @@ else
 fi
 
 ## Apply configuration to PostgreSQL
-NEW_ENTRY="local airflow_db airflow_user  password"
+NEW_ENTRY="local airflow_db airflow_user password"
 
 if ! grep -q "$NEW_ENTRY" "$PGDATA"/pg_hba.conf >/dev/null; then
-  psql -c "CREATE DATABASE airflow_db;" \
-       -c "CREATE USER airflow_user WITH PASSWORD 'airflow_pass';" \
-       -c "GRANT ALL PRIVILEGES ON DATABASE airflow_db TO airflow_user;" \
-       -c "USE airflow_db; GRANT ALL ON SCHEMA public TO airflow_user;" \ 
+  psql postgres -c "CREATE DATABASE airflow_db;" \
+        -c "CREATE USER airflow_user WITH PASSWORD 'airflow_pass';" \
+        -c "GRANT ALL PRIVILEGES ON DATABASE airflow_db TO airflow_user;"
+  psql airflow_db -c "GRANT ALL ON SCHEMA public TO airflow_user;"
     
   echo "$NEW_ENTRY" >> "$PGDATA"/pg_hba.conf
 fi
-# FIXME: Check if this is necessary or it can be reordered
+
 /usr/lib/postgresql/*/bin/pg_ctl -l "$PGDATA"/logfile restart
 wait-for-postgres
 
 ## Apply configuration to Airflow
 
-# Using configuration file
+printf  "\n==================  "
+printf  "\n Copying configuration file  \n"
+printf  "================== \n\n"
 if [[ -f "$CONFIGURATION" ]]; then
-    printf "\nCopying configuration file\n"
-    case "$CONFIGURATION" in
-        *.cfg)
-            # FIXME: copy the file into ${AIRFLOW_HOME}/airflow.cfg
-        *)
-            exit_err "File format not correct. Configuration must be specified in a *.cfg file."
-            ;;
-    esac
+    if [[ ! -d "$AIRFLOW_HOME" ]]; then
+        mkdir "$AIRFLOW_HOME"
+    fi
+    
+    cp "$CONFIGURATION" "${AIRFLOW_HOME}/airflow.cfg"
 fi
-
 # Using variables
+# AIRFLOW__CORE__SQL_ALCHEMY_CONN is getting deprecated
+# AIRFLOW__DATABASE__SQL_ALCHEMY_CONN will take it's place
+# For now, both are needed for a correct setup, this might change in the future
 declare -A airflow_variables=(
-    ["AIRFLOW_HOME"]=$AIRFLOW_HOME
-    ["AIRFLOW__CORE__EXECUTOR"]=$AIRFLOW__CORE__EXECUTOR
-    ["AIRFLOW__CORE__LOAD__EXAMPLES"]=$AIRFLOW__CORE__LOAD__EXAMPLES
-    ["AIRFLOW__CORE__DAGS_FOLDER"]=$AIRFLOW__CORE__DAGS_FOLDER
-    ["AIRFLOW__CORE__FERNET_KEY"]=$AIRFLOW__CORE__FERNET_KEY
+    ["AIRFLOW_HOME"]="$AIRFLOW_HOME"
+    ["AIRFLOW__CORE__EXECUTOR"]="$AIRFLOW__CORE__EXECUTOR"
+    ["AIRFLOW__CORE__LOAD__EXAMPLES"]="$AIRFLOW__CORE__LOAD__EXAMPLES"
+    ["AIRFLOW__CORE__DAGS_FOLDER"]="$AIRFLOW__CORE__DAGS_FOLDER"
+    ["AIRFLOW__CORE__FERNET_KEY"]="$AIRFLOW__CORE__FERNET_KEY"
+    ["AIRFLOW__CORE__SQL_ALCHEMY_CONN"]="postgresql+psycopg2://airflow_user:airflow_pass@localhost:5432/airflow_db"
     ["AIRFLOW__DATABASE__SQL_ALCHEMY_CONN"]="postgresql+psycopg2://airflow_user:airflow_pass@localhost:5432/airflow_db"
-    ["AIRFLOW__CELERY__BROKER_URL"]=$REDIS_URL
+    ["AIRFLOW__CELERY__BROKER_URL"]="$REDIS_URL"
 )
 
 # Export the variables if they are set
 for key in "${!airflow_variables[@]}"; do
     if [[ -n "${airflow_variables[$key]}" ]]; then
         export "$key"="${airflow_variables[$key]}"
-        echo "$key"="${airflow_variables[$key]}" >> >> ~/.bashrc
+        echo "$key"="${airflow_variables[$key]}" >> ~/.bashrc
     fi
 done
 
 # Switch from default SQLite to postgres
-airflow db migrate
+# airflow db migrate
 
 # FIXME: use only default credentials or let user choose?
 # Create an airflow webserver user
 airflow users create \
- --username admin \
- --firstname Admin \
- --lastname Admin \
+ --username "$USERNAME" \
+ --firstname FIRST_NAME \
+ --lastname LAST_NAME \
  --role Admin \
- --email admin@admin.com \
- --password admin
+ --password "$PASSWORD"  \
+ --email admin@example.org        
  
 # Start Airflow
-airflow webserver --port 8080 &
-airflow scheduler &
+#airflow webserver --port "$PORT" &
+#airflow scheduler &
 
-sleep infinity
+#sleep infinity
